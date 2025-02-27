6c2bce239f2f1adb06451b2bc6554d67ed4a46bd
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-02-27 21:46:39 +0530

    upgrade k8s

---
+++ tofu/kubernetes/upgrade-k8s.sh
@@ -0,0 +1,4 @@
+# Dry Run
+# talosctl --dry-run --nodes <controlplane node> upgrade-k8s --to 1.32.0 # renovate: github-releases=kubernetes/kubernetes
+
+talosctl --nodes <controlplane node> upgrade-k8s --to 1.32.0 # renovate: github-releases=kubernetes/kubernetes
